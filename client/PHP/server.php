1bc54ebddc488fcd1ba749e9d8bca70873735e09
WillTrem <wt22@videotron.ca>
2023-04-08 01:00:34 -0400

    Front end + backend end work together now

--- client/PHP/server.php
+++ client/PHP/server.php
@@ -1,3 +1,31 @@
 <?php
-    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+
+   $conn = require('dbConnection.php');
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    // print "Connected successfully". "<br>";
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    if($_POST){
+        $tableName = $_POST['tableName'];
+        $sql = "SELECT * FROM nbc353_4.$tableName;";
+        $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+      // output data of each row
+    //   while($row = $result->fetch_assoc()) {
+    //     print "id: " . $row["medicareNb"]. " - Name: " . $row["fName"]. " " . $row["lName"]. "<br>";
+    //   }
+      echo json_encode($result->fetch_all());
+    } else {
+      echo "0 results";
+    }
+    }
+   
+$conn->close();
 ?>
--- client/src/Components/siteBody.jsx
+++ client/src/Components/siteBody.jsx
@@ -1,7 +1,26 @@
 import Dropdown from 'react-bootstrap/Dropdown';
+import Table from 'react-bootstrap/Table';
 import { useState } from 'react';
+import axios from 'axios'
+
+
+
 const SiteBody = () => {
 	const [selectedTable, setSelectedTable] = useState('Choose your table')
+  const [queryResult, setQueryResult] = useState();
+  const handleOnDropdownSelect = (table) => {
+   
+    setSelectedTable(table);
+    axios({
+      method: 'post',
+      url: `http://localhost:8000/server.php`,
+      headers: { 'content-type': 'application/json' },
+      data: {
+        tableName: table
+      }
+    }).then((result) => {setQueryResult(result.data)})
+      .catch(error => console.log(error.message));
+  };
   return (<div>
 	<Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -10,10 +29,20 @@ const SiteBody = () => {
 
       <Dropdown.Menu>
 		{['Choose your table','Facilities', 'Employees', ].map((table) => {
-		  return(<Dropdown.Item onClick={() => {setSelectedTable(table)}}>{table}</Dropdown.Item>)
+		  return(<Dropdown.Item onClick={() => handleOnDropdownSelect(table)}>{table}</Dropdown.Item>)
 		})}
       </Dropdown.Menu>
     </Dropdown>
+    <Table>
+      <tbody>
+      {queryResult && queryResult.map((row) => { return <tr>
+        {row.map((attribute) => {
+          return <td>{attribute}</td>
+        })}
+        </tr>})}
+      </tbody>
+    </Table>
+    
   </div>);
 }
 export default SiteBody;
