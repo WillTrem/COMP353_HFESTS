1c9c2c674bc6b71479caea8fc688e7414321d521
WillTrem <wt22@videotron.ca>
2023-04-12 12:45:06 -0400

    Implemented addRow

---
+++ client/PHP/addRow.php
@@ -0,0 +1,34 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+
+   $conn = require('dbConnection.php');
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    // print "Connected successfully". "<br>";
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    if($_POST){
+        $tableName = $_POST['tableName'];
+        $row = $_POST['row'];
+        $keyList = array();
+        $valueList = array();
+
+        foreach($row as $key => $value){
+          array_push($keyList, $key);
+          array_push($valueList, "$value");
+        }
+        $joinedKeys= "(" . join(", ", $keyList) . ")";
+        $joinedValues= "(" . join(", ", $valueList) . ")";
+        
+        $sql = "INSERT INTO nbc353_4.$tableName $joinedKeys 
+                VALUES $joinedValues";
+        $result = $conn->query($sql);
+        echo $result;
+    }
+   
+$conn->close();
+?>
--- client/src/Components/siteBody.jsx
+++ client/src/Components/siteBody.jsx
@@ -4,6 +4,7 @@ import Spinner from 'react-bootstrap/Spinner';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { AiFillCloseSquare, AiFillEdit } from 'react-icons/ai';
+import { IoAddCircle } from 'react-icons/io5';
 import { Modal, Button, Form } from 'react-bootstrap';
 
 const SiteBody = () => {
@@ -12,8 +13,8 @@ const SiteBody = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [tableList, setTableList] = useState();
   const [rowEdit, setRowEdit] = useState();
-  if (!tableList) {
-  }
+  const [rowAdd, setRowAdd] = useState();
+
   useEffect(() => {
     axios({
       method: 'get',
@@ -49,7 +50,8 @@ const SiteBody = () => {
   };
 
   const handleModalClose = () => {
-    setRowEdit(undefined);
+    rowEdit && setRowEdit(undefined);
+    rowAdd && setRowAdd(undefined);
   };
 
   const handleEditModalSubmit = (e) => {
@@ -62,6 +64,26 @@ const SiteBody = () => {
     axios({
       method: 'post',
       url: `http://localhost:8000/PHP/editRowFrom${selectedTable}.php`,
+      headers: { 'content-type': 'application/json' },
+      data: {
+        row: fullData,
+      },
+    }).then((result) => {
+      console.log(result.data);
+    });
+    handleOnDropdownSelect(selectedTable);
+    handleModalClose();
+  };
+  const handleAddModalSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const formJson = Object.fromEntries(Array.from(formData.entries()).filter(([_, v]) => v !== ''));
+    const fullData = { ...rowEdit, ...formJson };
+
+    axios({
+      method: 'post',
+      url: `http://localhost:8000/PHP/addRow.php`,
       headers: { 'content-type': 'application/json' },
       data: {
         tableName: selectedTable,
@@ -90,7 +112,15 @@ const SiteBody = () => {
       {(isLoading && <Spinner></Spinner>) ||
         (queryResult && (
           <div className='flex flex-col gap-y-1'>
-            <span className='text-2xl font-bold'>{selectedTable}</span>
+            <div className='flex flex-row items-center gap-2'>
+              <span className='text-2xl font-bold'>{selectedTable}</span>
+              <IoAddCircle
+                className='h-7 w-7 text-green-600 hover:cursor-pointer hover:text-green-700'
+                onClick={() => {
+                  setRowAdd(Object.keys(queryResult[0]));
+                }}
+              ></IoAddCircle>
+            </div>
             <Table striped bordered hover>
               <thead>
                 <tr>
@@ -116,7 +146,7 @@ const SiteBody = () => {
                               }}
                             ></AiFillEdit>
                           </td>
-                          <td className='flex justify-center'>
+                          <td>
                             <AiFillCloseSquare className='block h-7 w-7 text-red-700 hover:cursor-pointer hover:text-red-800'></AiFillCloseSquare>
                           </td>
                         </tr>
@@ -127,9 +157,9 @@ const SiteBody = () => {
             </Table>
           </div>
         ))}
-      <Modal show={rowEdit} centered>
+      <Modal show={rowEdit} centered onHide={handleModalClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Edit row</Modal.Title>
+          <Modal.Title>Edit row in {selectedTable}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form className='flex flex-wrap gap-x-8 gap-y-4' onSubmit={handleEditModalSubmit}>
@@ -139,6 +169,30 @@ const SiteBody = () => {
                   <Form.Group>
                     <Form.Label>{attribute[0]}</Form.Label>
                     <Form.Control placeholder={attribute[1]} name={attribute[0]} />
+                  </Form.Group>
+                );
+              })}
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
+            <Button variant='secondary' onClick={handleModalClose}>
+              Close
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+      <Modal show={rowAdd} centered size='lg' onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add tuple in {selectedTable}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className='flex flex-wrap gap-x-6 gap-y-8 ' onSubmit={handleAddModalSubmit}>
+            {rowAdd &&
+              Object.entries(rowAdd).map((attribute) => {
+                return (
+                  <Form.Group>
+                    <Form.Label>{attribute[1]}</Form.Label>
+                    <Form.Control name={attribute[1]} />
                   </Form.Group>
                 );
               })}
